e6343fbc413c3252c95c9e2713b6a5527d2c8d50
tanloc <doantanloc04072004@gmail.com>
2024-06-29 18:48:08 +0700

    datcho

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DatChoCuaToi from "./Components/DatChoCuaToi.js";
 import Home from "./Components/Home.js";
 
 function App() {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
+      <DatChoCuaToi />
     </div>
   );
 }
